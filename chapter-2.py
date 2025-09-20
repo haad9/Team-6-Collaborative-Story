f402a84c412a873ebce8ecefa4796b5c7a6eedd7
RehamAfzal <reham.afzal1@gmail.com>
2025-09-19 20:47:50 -0500

    Reham Chapter-2.py

---
+++ chapter-2.py
@@ -0,0 +1,11 @@
+#chapter-2,py
+
+# The developer sat with there with his coffee thinkink about the next line of codes
+
+# The lack of sleep was finally catching upto him with the storm growing stronger
+
+def chaptertwo ():
+    print(" He typed and typed and typed. Errors after errors, and he felt like he was making no progress.")
+    print(" He finally broke through and deicded to do it on a new branch")
+
+chaptertwo()   
